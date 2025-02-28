7e6a9642b946feb76ce7396e96d2d2951dab3bb7
TriNgoc <caotringoc11@gmail.com>
2025-02-28 13:35:39 +0700

    user module

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+
+ConfigModule;
+MongooseModule;
+@Module({
+  imports: [
+    // MongooseModule.forRoot(
+    //   '',
+    // ),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
